ce4eda8bb882d4ddee279285f8eda005031c2809
Lc0606 <846008547@qq.com>
2022-04-12 20:42:32 +0800

    second commit

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -1,5 +1,6 @@
 public class GitTest {
     public static void main(String[] args) {
-        System.out.printf("Hello Git");
+        System.out.println("Hello Git");
+        System.out.println("Hello lc");
     }
 }
